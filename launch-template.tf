6c3b1d0bcec6c6b76ae385c8ca2e56d962f8c413
Ravi <ravi@gmail.com>
2022-10-19 13:05:29 +0530

    ec2 code

--- launch-template.tf
+++ launch-template.tf
@@ -27,18 +27,15 @@ resource "aws_launch_template" "main" {
       Name = local.TAG_PREFIX
     }
   }
-  user_data = filebase64(templatefile("${path.module}/userdata.sh", {
-
-
-
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     ENV                    = var.ENV
     COMPONENT              = var.COMPONENT
     DOCDB_ENDPOINT         = var.DOCDB_ENDPOINT
-    DOCDB_USER             = base64encode(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]),
-    DOCDB_PASS             = base64encode(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]),
-    RABBITMQ_USER_PASSWORD = base64encode(jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RABITMQ_USER_PASSWORD"]),
+    DOCDB_USER             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_USER"]
+    DOCDB_PASS             = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["DOCDB_PASS"]
+    RABBITMQ_USER_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RABITMQ_USER_PASSWORD"]
     MYSQL_ENDPOINT         = var.MYSQL_ENDPOINT
     REDDIS_ENDPOINT        = var.REDDIS_ENDPOINT
-  })
+  }))
 }
 //user_data = base64encode(templatefile("${path.module}/userdata.sh", {  
